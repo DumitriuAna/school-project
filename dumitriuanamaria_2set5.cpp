7b7900eebc81bd44b3f23ab7f15495fc71452956
DumitriuAna <44975229+DumitriuAna@users.noreply.github.com>
2018-11-29 16:50:02 +0200

    Add files via upload

---
+++ dumitriuanamaria_2set5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int n;
+int main()
+{
+    cout<<"n="; cin>>n;
+    int x=1;
+    while (n>0)
+    {
+        n=n>>1;
+        x=x+1;
+    }
+
+    cout<<x;
+    return 0;
+}
